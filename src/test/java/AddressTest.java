89fd26cb853878931f0de0781d3acfbf9f344ed5
yoon <yoonchan.rhie@gmail.com>
2025-12-08 13:49:51 -0500

    Created AddressTest class and added first unit test case.

--- src/test/java/AddressTest.java
+++ src/test/java/AddressTest.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.yoonchan.Address;
 
 public class AddressTest {
+    @Test
+    @DisplayName("isPostalCodeValid: 'A1B2C3' -> true")
+    void testIsPostalCodeValid() {
+        String input = "A1B2C3";
+        boolean expected = true;
+        boolean actual = Address.isPostalCodeValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
 }
