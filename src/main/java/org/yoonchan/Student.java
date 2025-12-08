94bb9a97d9eaeab66968a23d32dcccabf5089101
yoon <yoonchan.rhie@gmail.com>
2025-12-08 14:47:29 -0500

    Created the constructor for the Student class.

--- src/main/java/org/yoonchan/Student.java
+++ src/main/java/org/yoonchan/Student.java
@@ -11,4 +11,12 @@ public class Student {
     private ArrayList<Course> registeredCourses;
     private static int nextId = 1;
 
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("S%06d", nextId++);
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.registeredCourses = new ArrayList<Course>();
+    }
 }
