9f5b4b59b4d953f1002b2523305377d28dad56c5
yoon <yoonchan.rhie@gmail.com>
2025-12-08 14:12:44 -0500

    Changed comment on line 35 for better clarity.

--- src/main/java/org/yoonchan/Department.java
+++ src/main/java/org/yoonchan/Department.java
@@ -32,7 +32,7 @@ public class Department {
             return false;
         }
         for (char c : departmentName.toCharArray()) {
-            // 32 = space, 65 = A, 90 = Z, 97 = a, 122 = z
+            // 32 = ' ' (space), 65 = 'A', 90 = 'Z', 97 = 'a', 122 = 'z'
             if (!(c == 32 || c >= 65 && c <= 90 || c >= 97 && c <= 122)) {
                 return false;
             }
