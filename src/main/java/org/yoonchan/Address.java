ca7883dfa70692b30382edb3617843f6f98d8ccf
yoon <yoonchan.rhie@gmail.com>
2025-12-08 13:38:08 -0500

    Added required fields to the Address class.

--- src/main/java/org/yoonchan/Address.java
+++ src/main/java/org/yoonchan/Address.java
@@ -1,4 +1,9 @@
 package org.yoonchan;
 
 public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
 }
