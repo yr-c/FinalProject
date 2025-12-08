d9117f0ed44fc2614b3734b448c5c4450d3e404d
yoon <yoonchan.rhie@gmail.com>
2025-12-08 12:11:11 -0500

    Integrated the isDepartmentNameValid method into the constructor of the Department class.

--- src/main/java/org/yoonchan/Department.java
+++ src/main/java/org/yoonchan/Department.java
@@ -15,10 +15,19 @@ public class Department {
     private static int nextId = 1;
 
     public Department(String departmentId, String departmentName) {
-        this.departmentId = String.format("%04d", nextId++);
-        this.departmentName = departmentName;
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentId = String.format("%04d", nextId++);
+            this.departmentName = departmentName;
+        }
+        this.departmentId = null;
+        this.departmentName = null;
     }
 
+    /**
+     * Checks if a department name is valid. A department name is valid when it contains only letters and/or spaces.
+     * @param departmentName The input department name to be checked.
+     * @return Whether the department name is valid.
+     */
     public static boolean isDepartmentNameValid(String departmentName) {
         return true;
     }
