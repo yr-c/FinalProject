c30a4691410fbc21b55f892f0e6a4a5b4fc66eb6
yoon <yoonchan.rhie@gmail.com>
2025-12-08 12:17:50 -0500

    Added more unit tests to DepartmentTest.

--- src/test/java/DepartmentTest.java
+++ src/test/java/DepartmentTest.java
@@ -7,9 +7,89 @@ import org.yoonchan.Util;
 public class DepartmentTest {
     @Test
     @DisplayName("isDepartmentNameValid: 'Mathematics' -> true")
-    void testIsDepartmentNameValid() {
+    void testIsDepartmentNameValid1() {
         String input = "Mathematics";
         boolean expected = true;
+        boolean actual = Department.isDepartmentNameValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isDepartmentNameValid: 'Computer Science' -> true")
+    void testIsDepartmentNameValid2() {
+        String input = "Computer Science";
+        boolean expected = true;
+        boolean actual = Department.isDepartmentNameValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isDepartmentNameValid: 'Commerce$' -> false")
+    void testIsDepartmentNameValid3() {
+        String input = "Commerce$";
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isDepartmentNameValid: '?' -> false")
+    void testIsDepartmentNameValid4() {
+        String input = "?";
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isDepartmentNameValid: 'Computer_science' -> false")
+    void testIsDepartmentNameValid5() {
+        String input = "Computer_science";
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isDepartmentNameValid: ' History ' -> true")
+    void testIsDepartmentNameValid6() {
+        String input = " History ";
+        boolean expected = true;
+        boolean actual = Department.isDepartmentNameValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isDepartmentNameValid: '' -> false")
+    void testIsDepartmentNameValid7() {
+        String input = "";
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isDepartmentNameValid: ' ' -> false")
+    void testIsDepartmentNameValid8() {
+        String input = " ";
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isDepartmentNameValid: null -> false")
+    void testIsDepartmentNameValid() {
+        String input = null;
+        boolean expected = false;
         boolean actual = Department.isDepartmentNameValid(input);
 
         Assertions.assertEquals(expected,actual);
