3f84f4a3f681ce6e9fb896688f0895ceacb4292a
yoon <yoonchan.rhie@gmail.com>
2025-12-08 10:53:53 -0500

    Created toTitleCase method.

--- src/main/java/org/yoonchan/Util.java
+++ src/main/java/org/yoonchan/Util.java
@@ -1,4 +1,11 @@
 package org.yoonchan;
 
 public class Util {
+    public static String toTitleCase(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        str = str.toUpperCase();
+        return str.charAt(0) + str.substring(1).toLowerCase();
+    }
 }
