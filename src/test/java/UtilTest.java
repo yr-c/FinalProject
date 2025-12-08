7959115e1ac24bf4b22aac086d985b07fc002664
yoon <yoonchan.rhie@gmail.com>
2025-12-08 10:49:04 -0500

    Added more unit tests for the toTitleCase method.

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -5,10 +5,40 @@ import org.yoonchan.Util;
 
 public class UtilTest {
     @Test
-    @DisplayName("toTitleCase: hello -> Hello")
-    void testToTitleCase() {
+    @DisplayName("toTitleCase: 'hello' -> 'Hello'")
+    void testToTitleCase1() {
         String input = "hello";
         String expected = "Hello";
+        String actual = Util.toTitleCase(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("toTitleCase: hEllO -> Hello")
+    void testToTitleCase2() {
+        String input = "hEllO";
+        String expected = "Hello";
+        String actual = Util.toTitleCase(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("toTitleCase: '' -> ''")
+    void testToTitleCase3() {
+        String input = "";
+        String expected = "";
+        String actual = Util.toTitleCase(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("toTitleCase: null -> null")
+    void testToTitleCase4() {
+        String input = null;
+        String expected = null;
         String actual = Util.toTitleCase(input);
 
         Assertions.assertEquals(expected,actual);
