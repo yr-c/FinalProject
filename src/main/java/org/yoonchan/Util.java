f872e2466e7364ad8759161dd6a1dfbdeeba6f20
yoon <yoonchan.rhie@gmail.com>
2025-12-08 11:21:23 -0500

    Added documentation for toTitleCase method.

--- src/main/java/org/yoonchan/Util.java
+++ src/main/java/org/yoonchan/Util.java
@@ -1,6 +1,11 @@
 package org.yoonchan;
 
 public class Util {
+    /**
+     * Formats each word in a string to title case. Assumes words in str are always separated by space.
+     * @param str The input string to be formatted.
+     * @return The formatted string, in title case.
+     */
     public static String toTitleCase(String str) {
         if (str == null || str.isEmpty()) {
             return str;
