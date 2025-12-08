2057329258aa7c96d50aa57b1fac5327e92bf02e
yoon <yoonchan.rhie@gmail.com>
2025-12-08 14:00:06 -0500

    Added more test cases for the isPostalCodeValid method.

--- src/test/java/AddressTest.java
+++ src/test/java/AddressTest.java
@@ -6,9 +6,99 @@ import org.yoonchan.Address;
 public class AddressTest {
     @Test
     @DisplayName("isPostalCodeValid: 'A1B2C3' -> true")
-    void testIsPostalCodeValid() {
+    void testIsPostalCodeValid1() {
         String input = "A1B2C3";
         boolean expected = true;
+        boolean actual = Address.isPostalCodeValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: 'AAB2C3' -> false")
+    void testIsPostalCodeValid2() {
+        String input = "AAB2C3";
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: 'a1b2c3' -> true")
+    void testIsPostalCodeValid3() {
+        String input = "a1b2c3";
+        boolean expected = true;
+        boolean actual = Address.isPostalCodeValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: 'A1B2C3D4' -> false")
+    void testIsPostalCodeValid4() {
+        String input = "A1B2C3D4";
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: 'A1B2C$' -> false")
+    void testIsPostalCodeValid5() {
+        String input = "A1B2C$";
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: 'A1 B2C3' -> false")
+    void testIsPostalCodeValid6() {
+        String input = "A1 B2C3";
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: 'A B2C3' -> false")
+    void testIsPostalCodeValid7() {
+        String input = "A B2C3";
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: ' ' -> false")
+    void testIsPostalCodeValid8() {
+        String input = " ";
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: '' -> false")
+    void testIsPostalCodeValid9() {
+        String input = "";
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: null -> false")
+    void testIsPostalCodeValid10() {
+        String input = null;
+        boolean expected = false;
         boolean actual = Address.isPostalCodeValid(input);
 
         Assertions.assertEquals(expected,actual);
