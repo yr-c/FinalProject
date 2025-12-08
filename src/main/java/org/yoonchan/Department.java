a7cb22dd702153e186f5e0f8fb0cf1645adfc6ba
yoon <yoonchan.rhie@gmail.com>
2025-12-08 11:10:53 -0500

    Added fields, the constructor as well as the lombok equals, getter, setter, and toString to the Department class.

--- src/main/java/org/yoonchan/Department.java
+++ src/main/java/org/yoonchan/Department.java
@@ -1,4 +1,21 @@
 package org.yoonchan;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
 public class Department {
+    private String departmentId;
+    private String departmentName;
+    private static int nextId = 1;
+
+    public Department(String departmentId, String departmentName) {
+        this.departmentId = String.format("%04d", nextId++);
+        this.departmentName = departmentName;
+    }
 }
