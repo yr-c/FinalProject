5d589172a4069d01855ac358dc7b60b5ddcfe8ff
yoon <yoonchan.rhie@gmail.com>
2025-12-08 10:39:06 -0500

    Created first test instance for toTitleCase method.

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.yoonchan.Util;
 
 public class UtilTest {
+    @Test
+    @DisplayName("toTitleCase: hello -> Hello")
+    void testToTitleCase() {
+        String input = "hello";
+        String expected = "Hello";
+        String actual = Util.toTitleCase(input);
+
+        Assertions.assertEquals(expected,actual);
+    }
 }
