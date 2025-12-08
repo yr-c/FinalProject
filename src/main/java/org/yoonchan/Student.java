5585649b341445cd3c6c5ef9335c597799793380
yoon <yoonchan.rhie@gmail.com>
2025-12-08 14:42:23 -0500

    Added necessary fields to the Student class.

--- src/main/java/org/yoonchan/Student.java
+++ src/main/java/org/yoonchan/Student.java
@@ -1,4 +1,14 @@
 package org.yoonchan;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+    private static int nextId = 1;
+
 }
