b78dfa184eadebd5750fdd7c27b3969c645f47b7
yoon <yoonchan.rhie@gmail.com>
2025-12-08 15:38:06 -0500

    Created the constructor for the Assignment class.

--- src/main/java/org/yoonchan/Assignment.java
+++ src/main/java/org/yoonchan/Assignment.java
@@ -3,18 +3,23 @@ package org.yoonchan;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.util.ArrayList;
 
 @EqualsAndHashCode
 @Getter
 @Setter
-@ToString
 public class Assignment {
-    private String assignmentId; // This id should be increased automatically.
+    private String assignmentId;
     private String assignmentName;
     private double weight; // the sum of weights of all assignment should be 100
     private ArrayList<Integer> scores;
-    private static int nextId; // indicates the next ID that will be used
+    private static int nextId;
+
+    public Assignment(ArrayList<Integer> scores, double weight, String assignmentName) {
+        this.scores = scores;
+        this.weight = weight;
+        this.assignmentName = assignmentName;
+        this.assignmentId = String.format("%04d", nextId++);
+    }
 }
