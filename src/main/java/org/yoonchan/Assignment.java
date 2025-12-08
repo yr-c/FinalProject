27fec24b0094c34ed8c845da79d45c99ab880dec
yoon <yoonchan.rhie@gmail.com>
2025-12-08 15:35:54 -0500

    Added all necessary fields to the Assignment class, as well as lombok methods equals, getter, and setter.

--- src/main/java/org/yoonchan/Assignment.java
+++ src/main/java/org/yoonchan/Assignment.java
@@ -1,4 +1,20 @@
 package org.yoonchan;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
 public class Assignment {
+    private String assignmentId; // This id should be increased automatically.
+    private String assignmentName;
+    private double weight; // the sum of weights of all assignment should be 100
+    private ArrayList<Integer> scores;
+    private static int nextId; // indicates the next ID that will be used
 }
