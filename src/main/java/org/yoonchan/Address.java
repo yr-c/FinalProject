5021c8ccf6d30378dac53f319d3f2e736423cfb9
yoon <yoonchan.rhie@gmail.com>
2025-12-08 13:40:59 -0500

    Added lombok equals, getter, setter, and toString.

--- src/main/java/org/yoonchan/Address.java
+++ src/main/java/org/yoonchan/Address.java
@@ -1,5 +1,14 @@
 package org.yoonchan;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
 public class Address {
     private int streetNo;
     private String street;
