7f7b79e99a217245f7b9b580476875f19b25d280
Yoon <yoonchan.rhie@gmail.com>
2025-12-10 11:15:51 -0500

    Added fields to the Course class and created its constructor.

--- src/main/java/org/yoonchan/Course.java
+++ src/main/java/org/yoonchan/Course.java
@@ -1,4 +1,22 @@
 package org.yoonchan;
 
+import java.util.ArrayList;
+
 public class Course {
+    private String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private ArrayList<Assignment> assignments;
+    private ArrayList<Student> registeredStudents;
+    private static int nextId = 1;
+
+    public Course(String courseName, double credits, Department department, ArrayList<Assignment> assignments, ArrayList<Student> registeredStudents) {
+        this.courseId = String.format("C-%s-%02d", department.getDepartmentId(), nextId++);
+        this.courseName = courseName;
+        this.credits = credits;
+        this.department = department;
+        this.assignments = assignments;
+        this.registeredStudents = registeredStudents;
+    }
 }
