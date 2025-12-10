9299c0cbb53f9afac07cd8c4ed233d0c0544fbfe
Yoon <yoonchan.rhie@gmail.com>
2025-12-10 10:33:37 -0500

    Added Util.toTitleCase call for the student name.

--- src/main/java/org/yoonchan/Student.java
+++ src/main/java/org/yoonchan/Student.java
@@ -13,7 +13,7 @@ public class Student {
 
     public Student(String studentName, Gender gender, Address address, Department department) {
         this.studentId = String.format("S%06d", nextId++);
-        this.studentName = studentName;
+        this.studentName = Util.toTitleCase(studentName);
         this.gender = gender;
         this.address = address;
         this.department = department;
