d534477a5019f325ba04dc96366260a10d770704
yoon <yoonchan.rhie@gmail.com>
2025-12-12 14:31:25 -0500

    Created and wrote functionality for the generateRandomScore method in Assignment with javadoc.

--- src/main/java/org/yoonchan/Assignment.java
+++ src/main/java/org/yoonchan/Assignment.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 @EqualsAndHashCode
 @Getter
@@ -32,5 +33,28 @@ public class Assignment {
             sum += score;
         }
         System.out.println("Average score: " + (sum / scores.toArray().length));
+    }
+
+    /**
+     * Generates random scores for all students in an assignment,
+     * the scores are generated with the following rule: <p>
+     * Firstly generate a random number in range [0, 10], then <br>
+     * - if the number is 0, then generate a random score in range [0, 60) for the student; <br>
+     * - if the number is 1, 2, then generate a random score in range [60, 70) for the student; <br>
+     * - if the number is 3, 4, then generate a random score in range [70, 80) for the student; <br>
+     * - if the number is 5, 6, 7, 8, then generate a random score in range [80, 90) for the student; <br>
+     * - if the number is 9, 10, then generate a random score in range [90, 100] for the student;
+     */
+    public void generateRandomScore() {
+        Random random = new Random();
+        for (int i = 0; i < scores.toArray().length; i++) {
+            switch (random.nextInt(0, 11)) {
+                case 0 -> scores.add(i, random.nextInt(0, 60));
+                case 1, 2 -> scores.add(i, random.nextInt(60, 70));
+                case 3, 4 -> scores.add(i, random.nextInt(70, 80));
+                case 5, 6, 7, 8 -> scores.add(i, random.nextInt(80, 90));
+                case 9, 10 -> scores.add(i, random.nextInt(90, 101));
+            }
+        }
     }
 }
